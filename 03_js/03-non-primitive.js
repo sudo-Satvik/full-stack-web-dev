9bfe3faf38ade7459ac3e7202350be71048f6b09
Satvik <sharmasatvik43@gmail.com>
2024-12-06 00:55:42 +0530

    non primitive done

---
+++ 03_js/03-non-primitive.js
@@ -0,0 +1,26 @@
+// Array
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log("======================================");
+
+// Object
+let userProfile = {
+    name: "John Doe",
+    age: 30,
+    email: "i5ZVb@example.com"
+};
+console.log(userProfile);
+console.log(userProfile.email)
+console.log("======================================");
+
+// Function
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+console.log("======================================");
+// Function with default value
+function greet(name = "World") {
+    console.log(`Hello, ${name}!`);
+}
+    
