69c9164fddcd2bd9d25d480eff0666031ebb05df
Satvik <sharmasatvik43@gmail.com>
2024-12-26 22:30:25 +0530

    Add Animal constructor and prototype method examples

--- 03_js/15-constructor-function.js
+++ 03_js/15-constructor-function.js
@@ -13,10 +13,36 @@ function Car(make, model) {
 
 
 let myCar = new Car('Toyota', 'Corolla');
-console.log(myCar);
+// console.log(myCar);
 
 let myCar2 = new Car('Honda', 'Civic');
-console.log(myCar2);
+// console.log(myCar2);
 
 let me = new Person("Satvik", 21);
-console.log(me);
+// console.log(me);
+
+
+// we can assign nested function
+
+function Animal(species) {
+    this.species = species
+    this.eat = function() {
+        return `${this.species} is eating`;
+    }
+}
+
+let lion = new Animal("cat")
+
+// console.log(lion);
+// console.log(lion.eat());
+
+
+// Prototype in function is different than __proto__ or Object.getPrototypeOf()
+
+Animal.prototype.sound = function() {
+    return `${this.species} makes a sound!`
+}
+
+let germanSheperd = new Animal("dog");
+
+console.log(germanSheperd.sound());
