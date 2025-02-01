cdef15ab9649f3172dff62b26a3cbfef3cedd056
Satvik <sharmasatvik43@gmail.com>
2025-02-01 20:25:50 +0530

    Update index.html title, integrate Tailwind CSS, and implement Card component for game display

--- 04_react/v19_react_vite/01-reactjs/src/App.jsx
+++ 04_react/v19_react_vite/01-reactjs/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState, version } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Card from "./components/Card";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gray-800 w-full h-screen text-gray-50">
+      <h1 className="text-4xl font-semibold text-center pt-5">
+        Movie Card using Tailwind
+      </h1>
+      <div className="flex items-center justify-center mt-20">
+        <Card />
       </div>
-      <h1>Vite + React v{version}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
