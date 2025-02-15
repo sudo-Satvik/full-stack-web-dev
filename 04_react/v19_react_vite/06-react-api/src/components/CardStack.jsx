228e5f31d73a19c238fef81782baae595e377e74
Satvik Sharma <sharmasatvik43@gmail.com>
2025-02-16 01:12:41 +0530

    Refactor CardStack component to use async/await for API fetching; add loading and error states for improved user experience

--- 04_react/v19_react_vite/06-react-api/src/components/CardStack.jsx
+++ 04_react/v19_react_vite/06-react-api/src/components/CardStack.jsx
@@ -2,15 +2,38 @@ import { useEffect, useState } from "react";
 
 const CardStack = () => {
   const [api, setApi] = useState(null);
+  const [loadingState, setLoadingState] = useState(true);
+  const [errorState, setErrorState] = useState("");
+
   const API = "https://pokeapi.co/api/v2/pokemon/ditto";
 
-  const fetchApi = () => {
-    setTimeout(() => {
-      fetch(API)
-        .then((res) => res.json())
-        .then((data) => setApi(data))
-        .catch((err) => console.log(err));
-    }, 2000);
+  // const fetchApi = () => {
+  //   setTimeout(() => {
+  //     fetch(API)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setApi(data);
+  //         setLoadingState(false);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         setErrorState(err);
+  //         setLoadingState(false);
+  //       });
+  //   }, 1000);
+  // };
+
+  const fetchApi = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+      setApi(data);
+      setLoadingState(false);
+    } catch (err) {
+      console.log(err);
+      setErrorState(err);
+      setLoadingState(false);
+    }
   };
 
   useEffect(() => {
@@ -18,10 +41,16 @@ const CardStack = () => {
   }, []);
 
   // Error handling
-  if (!api)
+  if (loadingState)
     return (
       <div className="flex items-center justify-center h-200">
         <h1 className="text-6xl">Loading data, please wait....</h1>
+      </div>
+    );
+  if (errorState)
+    return (
+      <div className="flex items-center justify-center h-200">
+        <h1 className="text-6xl">Error: {errorState.message}</h1>
       </div>
     );
 
@@ -39,6 +68,17 @@ const CardStack = () => {
             />
           </figure>
           <p className="text-2xl font-bold capitalize mt-2">{api.name}</p>
+          <div className="grid grid-cols-3 gap-3">
+            <p className="bg-blue-500 p-3 text-center rounded-full cursor-pointer">
+              Height: <span>{api.height}</span>
+            </p>
+            <p className="bg-blue-500 p-3 text-center rounded-full cursor-pointer">
+              Weight: <span>{api.weight}</span>
+            </p>
+            <p className="bg-blue-500 p-3 text-center rounded-full cursor-pointer">
+              Speed: <span>{api.stats[5].base_stat}</span>
+            </p>
+          </div>
         </li>
       </ul>
     </div>
